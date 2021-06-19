bfba1ca66d539c5d35656310727520726b10e00d
venhvy <venhvy@gmail.com>
2021-06-19 22:45:12 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -30,7 +30,7 @@ const c: IConfig = {
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
-    <div style="font-weight: bold;">共收录\${total}个网站</div>
+    <div style="font-weight: bold;">共收录\${total}个公司</div>
     <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
   `,
 
