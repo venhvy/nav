eb74081cf290e9f1bdd2579e7a99534b2f97a40d
venhvy <venhvy@gmail.com>
2021-06-19 23:12:15 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -11,16 +11,16 @@ const c: IConfig = {
   hashMode: true,
 
   // 是否显示右上角的 Github 图标
-  showGithub: true,
+  showGithub: false,
 
   // 您的网站地址，有利于SEO
   homeUrl: 'https://nav3.cn',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '元璟数据 - 数据发现企业',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '数据发现企业',
 
   // 网站关键字
   keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
@@ -30,7 +30,7 @@ const c: IConfig = {
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
-    <div style="font-weight: bold;">共收录\${total}个公司</div>
+    <div style="font-weight: bold;">共发现\${total}个企业</div>
     <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
   `,
 
@@ -52,7 +52,7 @@ const c: IConfig = {
     posterImageUrls: [
       'https://raw.sevencdn.com/xjh22222228/nav/image/sim-wallpaper.jpg'
     ],
-    description: '这里收录多达 <b>${total}</b> 个优质网站， 助您工作、学习和生活'
+    description: '共发现了 <b>${total}</b> 个企业， 一起冒险、追求企业价值与远景'
   }
 }
 
